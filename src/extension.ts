6111a4a413f01b2fbf602253d61c8a9dcc23b58d
Tobias Fenster <tfenster@infoma.de>
2017-08-01 16:42:21 +0200

    first version finished

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,109 @@
 'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import {window, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument, TextEditor, workspace, TextLine} from 'vscode';
+
+const open = require('opn');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "navskills-webcast-17-08-01" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+export function activate(context: ExtensionContext) {
+    console.log('"ALRunner" is now active!');
+    
+    let alr = new ALRunner();
+    
+    let disp = commands.registerCommand('extension.runSelection', () => {
+        alr.runSelection(window.activeTextEditor);
     });
-
-    context.subscriptions.push(disposable);
+    
+    let disp2 = commands.registerCommand('extension.runFirstObject', () => {
+        alr.runFirstObject(window.activeTextEditor);
+    });
+    
+    let disp3 = commands.registerCommand('extension.publishAndRunSelection', () => {
+        alr.publishAndRunSelection(window.activeTextEditor);
+    });
+    
+    let disp4 = commands.registerCommand('extension.publishAndRunFirstObject', () => {
+        alr.publishAndRunFirstObject(window.activeTextEditor);
+    });
+    
+    context.subscriptions.push(disp);
+    context.subscriptions.push(disp2);
+    context.subscriptions.push(disp3);
+    context.subscriptions.push(disp4);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+}
+
+class ALRunner {
+    public runSelection(editor: TextEditor) {
+        // find the currently active line
+        let line = editor.document.lineAt(editor.selection.active.line);
+        
+        this.runObjectOnLine(line, false);
+    }
+    
+    public runFirstObject(editor: TextEditor) {
+        // find the first line
+        let line = editor.document.lineAt(0);
+        
+        this.runObjectOnLine(line, false);
+    }
+    
+    public publishAndRunSelection(editor: TextEditor) {
+        // find the currently active line
+        let line = editor.document.lineAt(editor.selection.active.line);
+        
+        this.runObjectOnLine(line, true);
+    }
+    
+    public publishAndRunFirstObject(editor: TextEditor) {
+        // find the first line
+        let line = editor.document.lineAt(0);
+        
+        this.runObjectOnLine(line, true);
+    }
+
+    private runObjectOnLine(line: TextLine, publish: boolean) {
+        if (line != null && line.text != null) {
+            //console.log('working on');
+            //console.log(line);
+            let lowertext = line.text.toLowerCase();
+            
+            // can only handle page or report objects
+            if (lowertext.startsWith('page ') || lowertext.startsWith('report')) {
+                if (lowertext.startsWith('report') && publish) {
+                    window.showErrorMessage('Publish and run is not supported for reports, only run without publish');
+                    return;
+                }
+                let tokens = lowertext.split(' ');
+                if (isNaN(Number(tokens[1]))) {
+                    window.showErrorMessage('Did not find an object number where I expected one. Are you at the first line of an object?');
+                } else {
+                    let config = workspace.getConfiguration('launch');
+                    let currentConfig = config.configurations;
+                    if (publish) {
+                        // found a valid object id, now change the launch config and call publish (which will also launch the object)
+                        currentConfig[0].startupObjectId = Number(tokens[1]);
+                        config.update('configurations', currentConfig);
+                        commands.executeCommand('al.publish');
+                    } else {
+                        // found a valid object id, now run it
+                        let server = currentConfig[0].server;
+                        let serverInstance = currentConfig[0].serverInstance;
+                        let objecttype = 'Page';
+                        if (lowertext.startsWith('report')) {
+                            objecttype = 'Report';
+                        }
+                        open(server + '/' + serverInstance + '/WebClient/default.aspx?' + objecttype + '=' + tokens[1]);
+                    }
+                }
+            } else {
+                window.showErrorMessage('Did not find a page or report object in the current line. Are you at the first line of a page or report or is the first line valid?')
+            }
+        }
+    }
 }
